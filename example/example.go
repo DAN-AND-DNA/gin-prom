8b5c913014d2a903c68f1f3c7a03aa466df9ad90
DAN-AND-DNA <247073805@qq.com>
2023-02-22 00:56:19 +0800

    指标测试

--- example/example.go
+++ example/example.go
@@ -5,8 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"log"
-	"net/http"
-	"net/http/httptest"
 	"time"
 )
 
@@ -17,25 +15,41 @@ func main() {
 
 	metrics := ginprom.NewMetrics("default", reg)
 
-	r.Use(ginprom.AddMetrics(metrics))
+	r.Use(ginprom.Export(metrics))
 
 	r.GET("/ping", func(c *gin.Context) {
+		time.Sleep(2 * time.Second)
 		c.JSON(200, gin.H{"message": "ok"})
 	})
 
-	go func() {
-		for {
-			// 30 qps
-			time.Sleep(30 * time.Millisecond)
-			w := httptest.NewRecorder()
-			req, _ := http.NewRequest("GET", "/ping", nil)
-			r.ServeHTTP(w, req)
-		}
-	}()
+	/*
+		go func() {
+			for {
+				// 30 qps
+				time.Sleep(30 * time.Millisecond)
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest("GET", "/ping", nil)
+				r.ServeHTTP(w, req)
+			}
+		}()
+
+	*/
 
 	go func() {
 		for {
 			time.Sleep(5 * time.Second)
+
+			/*
+				metric := &dto.Metric{}
+				o, err := metrics.PromDuration.MetricVec.GetMetricWithLabelValues([]string{"200", "/ping", "GET"}...)
+				if err != nil {
+					panic(err)
+				}
+
+				o.Write(metric)
+				fmt.Println(proto.MarshalTextString(metric))
+
+			*/
 			log.Printf("total_request: %d qps: %d \n", metrics.TotalRequest, metrics.QPS)
 		}
 	}()
--- middleware.go
+++ middleware.go
@@ -19,70 +19,75 @@ type Metrics struct {
 	QPS              int64 // 当前qps
 
 	// 当前请求总数
-	totalRequest *prometheus.CounterVec
-	uptime       prometheus.Counter
-	qps          prometheus.Gauge
-	duration     *prometheus.HistogramVec
+	PromTotalRequest     *prometheus.CounterVec
+	PromUptime           prometheus.Counter
+	PromQPS              prometheus.Gauge
+	PromDuration         *prometheus.HistogramVec
+	PromRequestSizeBytes *prometheus.SummaryVec
 }
 
 func NewMetrics(namespace string, registry *prometheus.Registry) *Metrics {
 	m := &Metrics{
 		// 总请求数
-		totalRequest: prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Namespace: fmt.Sprintf("gin_prom_%s", namespace),
-				Name:      "total_request",
-				Help:      "Total number of http requests made.",
-			}, []string{"status", "path", "method"},
-		),
+		PromTotalRequest: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Namespace: fmt.Sprintf("gin_prom_%s", namespace),
+			Name:      "total_request",
+			Help:      "Total number of http requests made.",
+		}, []string{"status", "path", "method"}),
 
-		// gin运行的时间
-		uptime: prometheus.NewCounter(
-			prometheus.CounterOpts{
-				Namespace: fmt.Sprintf("gin_prom_%s", namespace),
-				Name:      "uptime",
-				Help:      "Gin service uptime.",
-			},
-		),
+		// 总运行的时间
+		PromUptime: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: fmt.Sprintf("gin_prom_%s", namespace),
+			Name:      "uptime",
+			Help:      "Gin service uptime.",
+		}),
 
 		// 每秒的请求 (每60秒更新)
-		qps: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: fmt.Sprintf("gin_prom_%s", namespace),
-				Name:      "request_per_second",
-				Help:      "Request per second.",
-			},
-		),
+		PromQPS: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: fmt.Sprintf("gin_prom_%s", namespace),
+			Name:      "request_per_second",
+			Help:      "Request per second.",
+		}),
 
 		// 请求延迟分布
-		duration: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		PromDuration: prometheus.NewHistogramVec(prometheus.HistogramOpts{
 			Namespace: fmt.Sprintf("gin_prom_%s", namespace),
 			Name:      "requests_duration_seconds",
 			Help:      "Request latencies in seconds.",
-			Buckets:   []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.250, 0.5, 1},
+			Buckets:   []float64{0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 999},
+		}, []string{"status", "path", "method"}),
+
+		PromRequestSizeBytes: prometheus.NewSummaryVec(prometheus.SummaryOpts{
+			Namespace: fmt.Sprintf("gin_prom_%s", namespace),
+			Name:      "request_size_bytes",
+			Help:      "HTTP request sizes in bytes.",
 		}, []string{"status", "path", "method"}),
 	}
 
-	registry.MustRegister(m.totalRequest, m.uptime, m.qps, m.duration)
+	registry.MustRegister(m.PromTotalRequest, m.PromUptime, m.PromQPS, m.PromDuration)
 
-	// 计算和更新
+	// 计算、更新合
 	go func() {
 		for {
 			time.Sleep(time.Second)
 
 			m.Uptime++
-			m.uptime.Inc()
+			m.PromUptime.Inc()
 
+			// 3秒
 			if m.Uptime%3 == 0 {
-				// 每分钟的qps
 				if m.PrevTotalRequest == 0 {
 					m.PrevTotalRequest = m.TotalRequest
 					continue
 				}
 				m.QPS = int64(math.Ceil(float64(m.TotalRequest-m.PrevTotalRequest) / 3))
-
-				m.qps.Set(float64(m.QPS))
+				m.PromQPS.Set(float64(m.QPS))
 				m.PrevTotalRequest = m.TotalRequest
+			}
+
+			// 1小时
+			if m.Uptime%3600 == 0 {
+				m.PromDuration.Reset()
 			}
 
 		}
@@ -92,11 +97,11 @@ func NewMetrics(namespace string, registry *prometheus.Registry) *Metrics {
 }
 
 /*
-AddMetrics gin的中间件
+Export gin的中间件
 namespace 代表接口统计所属的命名空间，中间件可以统计不同的接口或者端口
 registry 代表 prometheus的 registry
 */
-func Observe(metrics *Metrics) gin.HandlerFunc {
+func Export(metrics *Metrics) gin.HandlerFunc {
 	if metrics == nil {
 		return nop
 	}
@@ -109,7 +114,7 @@ func Observe(metrics *Metrics) gin.HandlerFunc {
 		labels := []string{fmt.Sprintf("%d", c.Writer.Status()), c.Request.URL.Path, c.Request.Method}
 
 		metrics.TotalRequest++
-		metrics.totalRequest.WithLabelValues(labels...).Inc()
-		metrics.duration.WithLabelValues(labels...).Observe(time.Since(startTime).Seconds())
+		metrics.PromTotalRequest.WithLabelValues(labels...).Inc()
+		metrics.PromDuration.WithLabelValues(labels...).Observe(time.Since(startTime).Seconds())
 	}
 }
